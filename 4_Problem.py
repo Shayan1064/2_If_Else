a7012cba03909e35b2b382ecfe848c22535c63ce
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-02 14:39:24 +0500

    Chechking for positivity or nagetivity

---
+++ 4_Problem.py
@@ -0,0 +1,9 @@
+# Check whether a given number is positive, negative, or zero.
+
+number=int(input("Enter Number : "))
+if(number<0):
+    print("Number is nagetive")
+elif(number==0):
+    print("Number is Zero")
+else:
+    print("Number is Positive")
