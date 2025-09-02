3d519b7294375c1f803d95623b800823f9a527ae
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-02 14:40:03 +0500

    Checking for Plindrome

---
+++ 5_Probelm.py
@@ -0,0 +1,10 @@
+# Take an input string and check if it is a palindrome (same forward & backward).
+
+word=["S","m","S"]
+
+reverse_word=word[::-1]
+
+if(word==reverse_word):
+    print("Yes Plindrome")
+else:
+    print("Not Plindrome")
